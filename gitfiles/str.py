092374e3e03bbc5b925e892160c26e88600d7816
Palak Tailor <palaktailoredu@gmail.com>
2025-06-12 12:00:03 +0530

    newfiles

---
+++ gitfiles/str.py
@@ -0,0 +1,24 @@
+name = "palakjaya"
+# print(name.endswith("me"))
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(dir(str))
+# print(name.rstrip())
+# print(name.lstrip())
+# print(id(name))
+# print(name.split("k"))
+# print(name.partition("m"))
+# print(name.swapcase())
+# print(name.encode("ascii"))
+# print(name.removesuffix("ya"))
+# print(name.zfill(20))
+# print(name.center(30,'$'))
+# print(name.ljust(30,'#'))
+# print(name[1:-1])
+# text = "my name is\"jai\""
+# print(text)
+str = "r,l,p,d,f"
+str.count("r")
+
+
