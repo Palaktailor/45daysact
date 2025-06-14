d612f54983537dcf874b4ae00414b631d846f68d
Palak Tailor <palaktailoredu@gmail.com>
2025-06-14 11:54:17 +0530

    newfiles

--- basic.py
+++ basic.py
@@ -1,6 +1,8 @@
-a = 10
-b = True
-c = "hello"
-print(type(a))
-print(type(b))
-print(type(c))
+# a = 10
+# b = True
+# c = "hello"
+# print(type(a))
+# print(type(b))
+# # print(type(c))
+def addfunction(a,b):
+    return(a+b)
---
+++ test.py
@@ -0,0 +1,9 @@
+# a = 10
+# b = True
+# c = "hello"
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+def addfunction(a,b):
+    return a+b 
